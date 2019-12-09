64fb07ae9fe398df8cf7083991a2493d62988a04
Andreas Fieger <fiedsch@ja-eh.at>
2019-12-09 07:17:14 +0100

    fix labels

--- src/Resources/contao/languages/de/tl_calendar_events.php
+++ src/Resources/contao/languages/de/tl_calendar_events.php
@@ -1,19 +1,59 @@
 <?php
 
-// Wertelabels
+/*
+ * Wertelabels
+ */
 
 $GLOBALS['TL_LANG']['tl_calendar_events']['home'] = 'Heimspiel';
 $GLOBALS['TL_LANG']['tl_calendar_events']['away'] = 'Auswärtsspiel';
+
+/*
+ * Legenden
+ */
 $GLOBALS['TL_LANG']['tl_calendar_events']['nuLiga_legend'] = 'nuLiga';
-$GLOBALS['TL_LANG']['tl_calendar_events']['team'] = 'Mannschaft';
-$GLOBALS['TL_LANG']['tl_calendar_events']['roundName'] = 'Hin- oder Rückrunde';
-$GLOBALS['TL_LANG']['tl_calendar_events']['homeaway'] = 'Heim- oder Auswärtsspiel';
-$GLOBALS['TL_LANG']['tl_calendar_events']['courtHallName'] = 'Name der Spielstätte';
-$GLOBALS['TL_LANG']['tl_calendar_events']['teamHome'] = 'Heimmannschaft';
-$GLOBALS['TL_LANG']['tl_calendar_events']['teamGuest'] = 'Gastmannschaft';
-$GLOBALS['TL_LANG']['tl_calendar_events']['matchesHome'] = 'Tore Heimmannschaft';
-$GLOBALS['TL_LANG']['tl_calendar_events']['matchesGuest'] = 'Tore Gastmannschaft';
-$GLOBALS['TL_LANG']['tl_calendar_events']['meetingUuid'] = 'Uuid der Begegnung';
+
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['team'] = [
+    'Mannschaft',
+    'Name der Mannschaft (Daten aus nuLiga)'
+];
+$GLOBALS['TL_LANG']['tl_calendar_events']['roundName'] = [
+    'Hin- oder Rückrunde',
+    'Name der Runde (Daten aus nuLiga)'
+];
+$GLOBALS['TL_LANG']['tl_calendar_events']['homeaway'] = [
+    'Heim- oder Auswärtsspiel',
+    'Wo findet das Spiel statt? (Daten aus nuLiga)'
+];
+$GLOBALS['TL_LANG']['tl_calendar_events']['courtHallName'] = [
+    'Name der Spielstätte',
+    'Name der Spielstätte (Daten aus nuLiga)'
+];
+$GLOBALS['TL_LANG']['tl_calendar_events']['teamHome'] = [
+    'Heimmannschaft',
+    'Name der Heimmannschaft (Daten aus nuLiga)'
+];
+$GLOBALS['TL_LANG']['tl_calendar_events']['teamGuest'] = [
+    'Gastmannschaft',
+    'Name der Gastmannschaft (Daten aus nuLiga)'
+];
+$GLOBALS['TL_LANG']['tl_calendar_events']['matchesHome'] = [
+    'Tore Heimmannschaft',
+    'Tore der Heimmannschaft falls das Spiel bereits stattgefunden hat (Daten aus nuLiga)'
+];
+$GLOBALS['TL_LANG']['tl_calendar_events']['matchesGuest'] = [
+    'Tore Gastmannschaft',
+    'Tore der Gastmannschaft falls das Spiel bereits stattgefunden hat (Daten aus nuLiga)'
+];
+$GLOBALS['TL_LANG']['tl_calendar_events']['meetingUuid'] = [
+    'Uuid des Spiels',
+    'Eindeutige ID des Spiels (Daten aus nuLiga)'
+];
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['tstamp'] = [
+    'Zuletzt aktualisiert',
+    'Datum der letzten Aktualisierung (Daten aus nuLiga geholt)'
+];
 
 //$GLOBALS['TL_LANG']['tl_calendar_events']['xxx'] = [
 //    'Kurzlabel',

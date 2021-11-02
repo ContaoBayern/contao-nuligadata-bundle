c03e5280398e0b7ffe1a8193488d2b426ef7ef2f
Andreas Fieger <fiedsch@ja-eh.at>
2021-11-02 16:56:30 +0100

    Add season to team name

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -7,7 +7,8 @@ $GLOBALS['TL_DCA']['tl_content']['palettes']['nuligadata_table'] =
 $GLOBALS['TL_DCA']['tl_content']['fields']['team'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['team'],
     'inputType' => 'select',
-    'foreignKey' => 'tl_team.nu_name',
+    'foreignKey' => "tl_team.CONCAT(nu_name,', ',nu_season)",
+
     'eval' => ['tl_class'=>'w50', 'mandatory'=>true, 'includeBlankOption'=>true],
     'sql' => ['type' => 'integer', 'notnull' => true, 'unsigned' => true, 'default' => 0],
 ];
